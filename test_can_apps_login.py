d6579afd4e35716483793a33875c433e13cf0552
AlexKomanov <akomanov88@gmail.com>
2024-12-17 20:07:41 +0200

    changes inside test_can_apps_login.py

--- test_can_apps_login.py
+++ test_can_apps_login.py
@@ -2,6 +2,7 @@ from playwright.sync_api import Page, expect
 from faker import Faker
 fake = Faker()
 
+
 def test_example(page: Page):
     page.goto('https://canvusapps.com/login')
     page.locator('#email').fill(fake.email())
