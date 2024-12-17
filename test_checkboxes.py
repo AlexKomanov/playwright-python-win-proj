5f86caa4fe9bd2707df06a22c93f6a94568cf9c5
AlexKomanov <akomanov88@gmail.com>
2024-12-17 21:44:16 +0200

    adding github action

---
+++ test_checkboxes.py
@@ -0,0 +1,15 @@
+import re
+from playwright.sync_api import Page, expect
+
+
+def test_checkboxes(page: Page) -> None:
+    page.goto("https://www.qa-practice.com/elements/checkbox/mult_checkbox")
+    page.get_by_label("One").check()
+    page.get_by_label("Two").check()
+    page.get_by_label("Three").check()
+    page.get_by_label("Three").uncheck()
+    page.get_by_label("Three").check()
+    page.get_by_role("button", name="Submit").click()
+    expect(page.get_by_text("Selected checkboxes:")).to_be_visible()
+    expect(page.locator("#result-text")).to_contain_text("one, two, three")
+    expect(page.locator(".site_header")).to_be_visible()
---
+++ test_multiple_selects.py
@@ -0,0 +1,16 @@
+from playwright.sync_api import Page, expect
+
+
+def test_multiple_selects(page: Page) -> None:
+    page.goto("https://www.qa-practice.com/elements/select/mult_select")
+    page.get_by_label("Choose the place you want to").select_option("4")
+    print(page.get_by_label("Choose the place you want to").text_content())
+    print(page.get_by_label("Choose the place you want to").inner_text())
+    expect(page.get_by_label("Choose the place you want to")).to_have_value("4")
+    page.get_by_label("Choose how you want to get").select_option("1")
+    page.get_by_label("Choose when you want to go*").select_option("2")
+    expect(page.get_by_role("button", name="Submit")).to_be_visible()
+    page.get_by_role("button", name="Submit").click()
+    expect(page.get_by_text("You selected")).to_be_visible()
+    page.get_by_text("to go by car to the ocean").click()
+    expect(page.locator("#result-text")).to_contain_text("to go by car to the ocean tomorrow")
